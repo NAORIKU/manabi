41eebb90300071a546b3cdd10d39942248eebebe
NAORIKU <hakamatanaotaro@gmail.com>
2021-12-22 21:02:08 +0900

    add markdown stylings

--- src/components/entry.tsx
+++ src/components/entry.tsx
@@ -6,8 +6,16 @@ type Props = {
 };
 export default function ({issue}: Props) {
     const html = { __html: issue.bodyHTML };
-    return <div className="max-w-[620px] mx-auto">
-        <h1><a href={`/entry/${issue.id}`}>{issue.title}</a></h1>
-        <article dangerouslySetInnerHTML={html} />
-    </div>
+    return (<div className="max-w-[620px] mx-auto">
+        <h1 className="text-2xl font-bold mb-[16px]"><a href={`/entry/${issue.id}`}>{issue.title}</a></h1>
+        <div className="mb-[16px]">
+            <time className="text-xs text-gray-500" dateTime={issue.pubDate}>{toJST(issue.pubDate)}</time>
+        </div>
+        <article className="markdown mobile-portrait:markdown-sm" dangerouslySetInnerHTML={html} />
+    </div>)
+
+    function toJST(timetext: string): string {
+        const target = new Date(new Date(timetext).getTime() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
+        return target.getFullYear() + "-" + (target.getMonth() + 1) + "-" + target.getDate() + " " + target.getHours() + ":" + target.getMinutes() + ":" + target.getSeconds();
+    }
 }
